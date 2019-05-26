1b24dd857cb406e3d983c4f470ba5367f349423e
hana Charaabi <charaabihana90@gmail.com>
2019-05-26 07:11:03 +0100

    Updates

---
+++ precache-manifest.c63a0bae144f6e359baaf8c2077f6acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab670b2843eb38bfcd039fa9c50e3c6",
+    "url": "/movies-list-loading-hoc/index.html"
+  },
+  {
+    "revision": "d477207babed07f6e6a0",
+    "url": "/movies-list-loading-hoc/static/css/main.146da2ae.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/movies-list-loading-hoc/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "d477207babed07f6e6a0",
+    "url": "/movies-list-loading-hoc/static/js/main.2bf1993e.chunk.js"
+  },
+  {
+    "revision": "ec643bf512d48f1f1b48",
+    "url": "/movies-list-loading-hoc/static/js/runtime~main.7392689e.js"
+  },
+  {
+    "revision": "6f6cb7b084fa23e11c39010469bc77bd",
+    "url": "/movies-list-loading-hoc/static/media/spinner.6f6cb7b0.svg"
+  }
+]);
